f66acadaf773c1c66603ae78ca033264defcdf25
Jon Bringhurst <jon@bringhurst.org>
2011-09-28 09:59:57 -0600

    Split up the local file check functions more.

--- src/reaper/local.h
+++ src/reaper/local.h
@@ -7,5 +7,7 @@
 
 static unsigned long int reaper_strtoul(const char *nptr, int *ret_code);
 void reaper_check_local_queue(CIRCLE_handle *handle, char *key);
+long int reaper_mtime_to_number(char *mtime_str);
+void reaper_check_and_delete_file(char *filename, long int db_mtime);
 
 #endif /* LOCAL_H */
