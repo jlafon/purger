6341c94d2f00178eca6df786ef76d2a69b80402b
Jon Bringhurst <jon@bringhurst.org>
2011-09-28 10:25:10 -0600

    Cleaned up code that deceides if the file should be marked as expired.

--- src/reaper/local.h
+++ src/reaper/local.h
@@ -9,5 +9,6 @@ static unsigned long int reaper_strtoul(const char *nptr, int *ret_code);
 void reaper_check_local_queue(CIRCLE_handle *handle, char *key);
 long int reaper_mtime_to_number(char *mtime_str);
 void reaper_check_and_delete_file(char *filename, long int db_mtime);
+int reaper_is_file_expired(long int old_db_mtime, long int new_stat_mtime, int age_allowed_in_days, char *filename);
 
 #endif /* LOCAL_H */
