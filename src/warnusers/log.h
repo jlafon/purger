b32cb28cabec0349ccee834165c9bafc770a24ee
Jharrod LaFon <jlafon@lanl.gov>
2011-09-28 16:15:17 -0600

    Still working on warn users.

--- src/warnusers/log.h
+++ src/warnusers/log.h
@@ -14,15 +14,15 @@
 #define LOG_DBG   (5)
 
 #define LOG(level, ...) do {  \
-        if (level <= CIRCLE_debug_level) { \
-            fprintf(CIRCLE_debug_stream,"%d:%s:%d:",CIRCLE_global_rank, __FILE__, __LINE__); \
-            fprintf(CIRCLE_debug_stream, __VA_ARGS__); \
-            fprintf(CIRCLE_debug_stream, "\n"); \
-            fflush(CIRCLE_debug_stream); \
+        if (level <= WARNUSERS_debug_level) { \
+            if(level == LOG_DBG) fprintf(WARNUSERS_debug_stream,"%d:%s:%d: ",CIRCLE_global_rank, __FILE__, __LINE__); \
+            fprintf(WARNUSERS_debug_stream, __VA_ARGS__); \
+            fprintf(WARNUSERS_debug_stream, "\n"); \
+            fflush(WARNUSERS_debug_stream); \
         } \
     } while (0)
 
-extern FILE *CIRCLE_debug_stream;
-extern int  CIRCLE_debug_level;
+extern FILE *WARNUSERS_debug_stream;
+extern int  WARNUSERS_debug_level;
 extern int CIRCLE_global_rank;
 #endif /* LOG_H */
--- src/warnusers/warnusers.c
+++ src/warnusers/warnusers.c
@@ -7,14 +7,14 @@
 #include <ctype.h>
 #include <time.h>
 
+#include "state.h"
 #include "warnusers.h"
 #include "log.h"
-#include "sprintstatf.h"
-#include "hash.h"
 
 #include <hiredis.h>
 #include <async.h>
-
+FILE *WARNUSERS_debug_stream;
+int WARNUSERS_debug_level;
 char         *TOP_DIR;
 redisContext *REDIS;
 
@@ -25,58 +25,92 @@ void
 add_objects(CIRCLE_handle *handle)
 {
     char buf[256];
-    sprintf(buf,"%d",warnusers_redis_run_spop());
+    if(warnusers_redis_run_spop(buf)< 0)
+    {
+        LOG(LOG_WARN,"No elements in set.");
+        int status = warnusers_redis_run_get("treewalk");
+        if(status == 0)
+        {
+            LOG(LOG_WARN,"Treewalk no longer running, and the set is empty.  Exiting warnusers.");
+            exit(0);
+        }
+        else if (status == 1)
+        {
+            LOG(LOG_WARN,"Treewalk is running, but the set is empty.  Warnusers will now spinlock waiting for set elements.");
+            while(warnusers_redis_run_scard("warnlist") <= 0 && warnusers_redis_run_get("treewalk") == 1)
+                ;
+            if(warnusers_redis_run_spop(buf)< 0)
+            {
+                LOG(LOG_WARN,"Warnusers giving up.  No items in the set.");
+                exit(0);
+            }
+        }
+    }
     handle->enqueue(buf);
 }
 
-void
-process_objects(CIRCLE_handle *handle)
+void warnusers_get_uids(CIRCLE_handle *handle)
 {
-    if(CIRCLE_global_rank == 0)
+    int i = 0;
+    char uid[256];
+    int status = warnusers_redis_run_get("treewalk");
+    switch(status)
     {
-        int count = warnusers_redis_run_scard("warnlist");
-        LOG(LOG_DBG,"Cardinality of warnlist is %d\n",count);
-        if(count <= 0)
-        {
-            int status = warnusers_redis_run_get("treewalk");
-            if(status == 0)
+        case 0:
+            LOG(LOG_DBG,"Treewalk no longer running.");
+            if(warnusers_redis_run_scard("warnlist") < 0)
             {
-                LOG(LOG_DBG,"Treewalk no longer running, and set is empty.");
+                LOG(LOG_WARN,"Treewalk is not running, and the set is empty. Exiting.");
+                exit(0);
             }
-            else if(status == 1)
-            {
-                LOG(LOG_DBG,"Treewalk is still running, but set is empty.");
-            }
-            else
-            {
-                LOG(LOG_ERR,"Treewalk key not set.  Unexpected state.");
-            }
-            return;
-        }
-        else
-        {
-            int i = 0;
-            char uid[256];
-            for(i = 0; i < count; i++)
+            for(i=0; i < warnusers_redis_run_scard("warnlist"); i++)
             {
                 if(warnusers_redis_run_spop(uid) == -1)
                 {
-                    LOG(LOG_ERR,"SPOP returned -1, but there should have been elements in the set.");
-                    break;
+                    LOG(LOG_ERR,"Something went badly wrong with the uid set.");
+                    exit(0);
                 }
-                if(handle->enqueue(uid) == -1)
-                {
-                    LOG(LOG_WARN,"Unable to add item to libcircle queue, placing item back in the warnlist set");
-                    warnusers_redis_run_sadd(uid);
-                    return;
-                }
+                handle->enqueue(uid);
             }
-        }
+
+            
+            break;
+        case 1:
+            if(warnusers_redis_run_scard("warnlist") < 0)
+                LOG(LOG_WARN,"Treewalk is running, but the set is empty.  Warnusers will now spinlock waiting for set elements, or for treewalk to finish.");
+            
+            /* Spin lock */
+            while(warnusers_redis_run_scard("warnlist") <= 0 && warnusers_redis_run_get("treewalk") == 1)
+                ;
+            for(i=0; i < warnusers_redis_run_scard("warnlist"); i++)
+            {
+                if(warnusers_redis_run_spop(uid) == -1)
+                {
+                    LOG(LOG_ERR,"Something went badly wrong with the uid set.");
+                    exit(0);
+                }
+                handle->enqueue(uid);
+            }
+            break;
+        default:
+            LOG(LOG_ERR,"Treewalk key not set.  Unexpected state.");
+            break;
+    }
+}
+void
+process_objects(CIRCLE_handle *handle)
+{
+    char uid[256];
+    LOG(LOG_DBG,"Rank %d in process_objects",CIRCLE_global_rank);
+    if(CIRCLE_global_rank == 0)
+    {
+        warnusers_get_uids(handle);
     }
     else
     {
         char temp[CIRCLE_MAX_STRING_LEN];
         /* Pop an item off the queue */ 
+        LOG(LOG_DBG, "Popping, queue has %d elements", handle->local_queue_size());
         handle->dequeue(temp);
         LOG(LOG_DBG, "Popped [%s]", temp);
     }
@@ -89,24 +123,28 @@ warnusers_redis_run_sadd(int id)
     sprintf(buf,"SADD warnlist %d",id);
     warnusers_redis_run_cmd(buf,buf);
 }
-void
+int
 warnusers_redis_run_cmd(char *cmd, char *filename)
 {
     LOG(LOG_DBG, "RedisCmd = \"%s\"", cmd);
-
-    if(redisCommand(REDIS, cmd) != NULL)
+    redisReply *reply = redisCommand(REDIS, cmd);
+    if(reply->type != REDIS_REPLY_ERROR)
     {   
-        LOG(LOG_DBG, "Sent %s to redis", filename);
+        LOG(LOG_DBG, "Sent %s to redis", cmd);
     }   
     else
     {   
-        LOG(LOG_DBG, "Failed to SET %s", filename);
+        LOG(LOG_DBG, "Failed %s: %s", cmd,reply->str);
         if (REDIS->err)
         {   
             LOG(LOG_ERR, "Redis error: %s", REDIS->errstr);
+            return -1; 
         }   
+    
     }   
+    return 0;
 }
+
 
 int
 warnusers_redis_run_scard(char * set)
@@ -123,7 +161,7 @@ warnusers_redis_run_scard(char * set)
     }
     else if(getReply->type == REDIS_REPLY_INTEGER)
     {
-        LOG(LOG_DBG,"GET returned an int.");
+        LOG(LOG_DBG,"GET returned an int: %d.",getReply->integer);
         return getReply->integer;
     }
     else
@@ -165,6 +203,74 @@ warnusers_redis_run_spop(char * uid)
    return 0;
 }
 
+int
+warnusers_redis_run_get_str(char * key, char * str)
+{
+    char * redis_cmd_buf = (char*)malloc(2048*sizeof(char));
+    sprintf(redis_cmd_buf, "GET %s",key);
+    redisReply *getReply = redisCommand(REDIS,redis_cmd_buf);
+    if(getReply->type == REDIS_REPLY_NIL)
+        return -1;
+    else if(getReply->type == REDIS_REPLY_STRING)
+    {
+        LOG(LOG_DBG,"GET returned a string \"%s\"\n", getReply->str);
+        strcpy(str,getReply->str);
+        return 0;
+    }
+    else
+        LOG(LOG_DBG,"GET didn't return a string.");
+    return -1;
+}
+
+
+int
+warnusers_check_state(int rank, int force)
+{
+   char * getCmd = (char *) malloc(sizeof(char)*256);
+   sprintf(getCmd,"PURGER_STATE");
+   int status = warnusers_redis_run_get(getCmd);
+   int transition_check = purger_state_check(status,PURGER_STATE_WARNUSERS); 
+   if(transition_check == PURGER_STATE_R_NO)
+   {
+       if(rank == 0) LOG(LOG_FATAL,"Error: You cannot run warnusers at this time.  You can only run warnusers after a previous warnusers, or after treewalk has been run.");
+       return -1;
+   }
+   else if(transition_check == PURGER_STATE_R_FORCE && !force)
+   {
+       if(rank == 0) LOG(LOG_ERR,"Error: You are asking warnusers to run, but not after treewalk has run.  If you are sure you want to do this, run again with -f to force.");
+       return -1;
+   }
+   else if(transition_check == PURGER_STATE_R_YES)
+   {
+       if(rank == 0)
+       {
+           char time_stamp[256];
+           warnusers_redis_run_get_str("warnusers_timestamp",time_stamp);
+           LOG(LOG_INFO,"Warnusers starting normally. Last successfull warnusers: %s",time_stamp);
+       }
+   }
+   sprintf(getCmd,"warnusers-rank-%d",rank);
+   status = warnusers_redis_run_get(getCmd);
+   sprintf(getCmd,"set warnusers-rank-%d 1", rank);
+    if(status == 1 && !force)
+    {
+        if(rank == 0) LOG(LOG_ERR,"Warnusers is already running.  If you wish to continue, verify that there is not a warnusers already running and re-run with -f to force it.");
+        return -1;
+    }
+    if(rank == 0 && warnusers_redis_run_cmd(getCmd,"")<0)
+    {
+        LOG(LOG_ERR,"Unable to %s",getCmd);
+        return -1;
+    }
+   sprintf(getCmd,"set PURGER_STATE %d",PURGER_STATE_TREEWALK);
+   if(rank == 0 && warnusers_redis_run_cmd(getCmd,"")<0)
+    {
+        LOG(LOG_ERR,"Unable to %s",getCmd);
+        return -1;
+    }
+  return 0;
+}
+
 
 void
 print_usage(char **argv)
@@ -183,13 +289,14 @@ main (int argc, char **argv)
 
     int redis_hostname_flag = 0;
     int redis_port_flag = 0;
+    int force_flag = 0;
 
-    /* Enable logging. */
-   // dbgstream = stderr;
-   // debug_level = PURGER_LOGLEVEL;
+    WARNUSERS_debug_stream = stdout;
+    WARNUSERS_debug_level = 4;
 
+    int rank = CIRCLE_init(argc, argv);
     opterr = 0;
-    while((c = getopt(argc, argv, "h:p:")) != -1)
+    while((c = getopt(argc, argv, "h:p:l:f")) != -1)
     {
         switch(c)
         {
@@ -203,8 +310,16 @@ main (int argc, char **argv)
                 redis_port_flag = 1;
                 break;
 
+            case 'f':
+                force_flag = 1;
+                if(rank == 0) LOG(LOG_WARN,"Warning: You have chosen to force warnusers.");
+                break; 
+            case 'l':
+                WARNUSERS_debug_level = atoi(optarg);
+                break;
+
             case '?':
-                if (optopt == 'd' || optopt == 'h' || optopt == 'p')
+                if (optopt == 'd' || optopt == 'h' || optopt == 'p' || optopt == 'l')
                 {
                     print_usage(argv);
                     fprintf(stderr, "Option -%c requires an argument.\n", optopt);
@@ -233,13 +348,13 @@ main (int argc, char **argv)
 
     if(redis_hostname_flag == 0)
     {
-        LOG(LOG_WARN, "A hostname for redis was not specified, defaulting to localhost.");
+        if(rank == 0) LOG(LOG_WARN, "A hostname for redis was not specified, defaulting to localhost.");
         redis_hostname = "localhost";
     }
 
     if(redis_port_flag == 0)
     {
-        LOG(LOG_WARN, "A port number for redis was not specified, defaulting to 6379.");
+        if(rank == 0) LOG(LOG_WARN, "A port number for redis was not specified, defaulting to 6379.");
         redis_port = 6379;
     }
 
@@ -254,18 +369,36 @@ main (int argc, char **argv)
     }
 
     time(&time_started);
-    CIRCLE_init(argc, argv);
-    CIRCLE_cb_create(&add_objects);
+    
+    if(warnusers_check_state(rank,force_flag) < 0)
+        exit(1);
     CIRCLE_cb_process(&process_objects);
     CIRCLE_begin();
     CIRCLE_finalize();
     time(&time_finished);
-
-    //LOG(LOG_INFO, "warnusers run started at: %l", time_started);
-    //LOG(LOG_INFO, "warnusers run completed at: %l", time_finished);
-    //LOG(LOG_INFO, "warnusers total time (seconds) for this run: %l",
-    //    ((double) (time_finished - time_started)) / CLOCKS_PER_SEC);
-
+    char starttime_str[256];
+    char endtime_str[256];
+    struct tm * localstart = localtime( &time_started );
+    struct tm * localend = localtime ( &time_finished );
+    strftime(starttime_str, 256, "%b-%d-%Y,%H:%M:%S",localstart);
+    strftime(endtime_str, 256, "%b-%d-%Y,%H:%M:%S",localend);
+    char getCmd[256];
+    sprintf(getCmd,"set warnusers_timestamp \"%s\"",endtime_str);
+    if(warnusers_redis_run_cmd(getCmd,getCmd)<0)
+    {   
+         LOG(LOG_ERR,"Unable to %s",getCmd);
+    }  
+    sprintf(getCmd,"set warnusers-rank-%d 0", rank);
+    if(warnusers_redis_run_cmd(getCmd,getCmd)<0)
+    {
+         LOG(LOG_ERR,"Unable to %s",getCmd);
+    }
+    if(rank == 0)
+    {
+        LOG(LOG_INFO, "Warnusers run started at: %s", starttime_str);
+        LOG(LOG_INFO, "Warnusers run completed at: %s", endtime_str);
+        LOG(LOG_INFO, "Warnusers total time (seconds) for this run: %f",difftime(time_finished,time_started));
+    }
     exit(EXIT_SUCCESS);
 }
 
