32ab260c22d66c5c8c13ac7a51d1527854e07df3
Jon Bringhurst <jon@bringhurst.org>
2011-10-03 18:39:47 -0600

    Converted to common logging.

--- src/treewalk/treewalk.c
+++ src/treewalk/treewalk.c
@@ -9,16 +9,18 @@
 
 #include "state.h"
 #include "treewalk.h"
-#include "log.h"
 #include "sprintstatf.h"
 #include "hash.h"
+
+#include "../common/log.h"
 
 #include <hiredis.h>
 #include <async.h>
 #include <mpi.h>
 
-FILE* TREEWALK_debug_stream;
-int TREEWALK_debug_level;
+FILE* PURGER_debug_stream;
+PURGER_loglevel PURGER_debug_level;
+int PURGER_global_rank;
 char         *TOP_DIR;
 redisContext *REDIS;
 
@@ -58,14 +60,14 @@ process_objects(CIRCLE_handle *handle)
     stat_time[1] += MPI_Wtime()-stat_time[0];
     if(status != EXIT_SUCCESS)
     {
-            LOG(LOG_ERR, "Error: Couldn't stat \"%s\"", temp);
+            LOG(PURGER_LOG_ERR, "Error: Couldn't stat \"%s\"", temp);
     }
     /* Check to see if it is a directory.  If so, put its children in the queue */
     else if(S_ISDIR(st.st_mode) && !(S_ISLNK(st.st_mode)))
     {
         current_dir = opendir(temp);
         if(!current_dir) {
-            LOG(LOG_ERR, "Unable to open dir: %s",temp);
+            LOG(PURGER_LOG_ERR, "Unable to open dir: %s",temp);
         }
         else
         {
@@ -80,7 +82,7 @@ process_objects(CIRCLE_handle *handle)
                     strcat(stat_temp,"/");
                     strcat(stat_temp,current_ent->d_name);
 
-                    LOG(LOG_DBG, "Pushing [%s] <- [%s]", stat_temp, temp);
+                    LOG(PURGER_LOG_DBG, "Pushing [%s] <- [%s]", stat_temp, temp);
                     handle->enqueue(&stat_temp[0]);
                 }
             }
@@ -109,7 +111,7 @@ process_objects(CIRCLE_handle *handle)
            to warnlist */
         if(difftime(time_started,st.st_mtime) > expire_threshold)
         {
-            LOG(LOG_DBG,"File expired: \"%s\"",temp);
+            LOG(PURGER_LOG_DBG,"File expired: \"%s\"",temp);
             /* The mtime of the file as a zadd. */
             treewalk_redis_run_zadd(filekey, (long)st.st_mtime, "mtime", temp);
             /* add user to warn list */
@@ -188,18 +190,18 @@ treewalk_create_redis_attr_cmd(char *buf, struct stat *st, char *filename, char 
 int
 treewalk_redis_run_cmd(char *cmd, char *filename)
 {
-    LOG(LOG_DBG, "RedisCmd = \"%s\"", cmd);
+    LOG(PURGER_LOG_DBG, "RedisCmd = \"%s\"", cmd);
     redisReply *reply = redisCommand(REDIS, cmd);
     if(reply->type != REDIS_REPLY_ERROR)
     {
-        LOG(LOG_DBG, "Sent %s to redis", cmd);
+        LOG(PURGER_LOG_DBG, "Sent %s to redis", cmd);
     }
     else
     {
-        LOG(LOG_DBG, "Failed %s: %s", cmd,reply->str);
+        LOG(PURGER_LOG_DBG, "Failed %s: %s", cmd,reply->str);
         if (REDIS->err)
         {
-            LOG(LOG_ERR, "Redis error: %s", REDIS->errstr);
+            LOG(PURGER_LOG_ERR, "Redis error: %s", REDIS->errstr);
             return -1;
         }
     
@@ -217,12 +219,12 @@ treewalk_redis_run_get_str(char * key, char * str)
         return -1;
     else if(getReply->type == REDIS_REPLY_STRING)
     {
-        LOG(LOG_DBG,"GET returned a string \"%s\"\n", getReply->str);
+        LOG(PURGER_LOG_DBG,"GET returned a string \"%s\"\n", getReply->str);
         strcpy(str,getReply->str);
         return 0;
     }
     else
-        LOG(LOG_DBG,"GET didn't return a string.");
+        LOG(PURGER_LOG_DBG,"GET didn't return a string.");
     return -1;
 }
 
@@ -236,11 +238,11 @@ treewalk_redis_run_get(char * key)
         return -1;
     else if(getReply->type == REDIS_REPLY_STRING)
     {
-        LOG(LOG_DBG,"GET returned a string \"%s\"\n", getReply->str);
+        LOG(PURGER_LOG_DBG,"GET returned a string \"%s\"\n", getReply->str);
         return atoi(getReply->str);
     }
     else
-        LOG(LOG_DBG,"GET didn't return a string.");
+        LOG(PURGER_LOG_DBG,"GET didn't return a string.");
     return -1;
 }
 
@@ -264,12 +266,12 @@ treewalk_check_state(int rank, int force)
    int transition_check = purger_state_check(status,PURGER_STATE_TREEWALK); 
    if(transition_check == PURGER_STATE_R_NO)
    {
-       if(rank == 0) LOG(LOG_FATAL,"Error: You cannot run treewalk at this time.  You can only run treewalk after a previous treewalk, or after reaper has been run.");
+       if(rank == 0) LOG(PURGER_LOG_FATAL,"Error: You cannot run treewalk at this time.  You can only run treewalk after a previous treewalk, or after reaper has been run.");
        return -1;
    }
    else if(transition_check == PURGER_STATE_R_FORCE && !force)
    {
-       if(rank == 0) LOG(LOG_ERR,"Error: You are asking treewalk to run, but not after reaper has run.  If you are sure you want to do this, run again with -f to force.");
+       if(rank == 0) LOG(PURGER_LOG_ERR,"Error: You are asking treewalk to run, but not after reaper has run.  If you are sure you want to do this, run again with -f to force.");
        return -1;
    }
    else if(transition_check == PURGER_STATE_R_YES)
@@ -278,7 +280,7 @@ treewalk_check_state(int rank, int force)
        {
            char time_stamp[256];
            treewalk_redis_run_get_str("treewalk_timestamp",time_stamp);
-           LOG(LOG_INFO,"Treewalk starting normally. Last successfull treewalk: %s",time_stamp);
+           LOG(PURGER_LOG_INFO,"Treewalk starting normally. Last successfull treewalk: %s",time_stamp);
        }
    }
    sprintf(getCmd,"treewalk-rank-%d",rank);
@@ -286,18 +288,18 @@ treewalk_check_state(int rank, int force)
    sprintf(getCmd,"set treewalk-rank-%d 1", rank);
     if(status == 1 && !force)
     {
-        if(rank == 0) LOG(LOG_ERR,"Treewalk is already running.  If you wish to continue, verify that there is not a treewalk already running and re-run with -f to force it.");
+        if(rank == 0) LOG(PURGER_LOG_ERR,"Treewalk is already running.  If you wish to continue, verify that there is not a treewalk already running and re-run with -f to force it.");
         return -1;
     }
     if(rank == 0 && treewalk_redis_run_cmd(getCmd,"")<0)
     {
-        LOG(LOG_ERR,"Unable to %s",getCmd);
+        LOG(PURGER_LOG_ERR,"Unable to %s",getCmd);
         return -1;
     }
    sprintf(getCmd,"set PURGER_STATE %d",PURGER_STATE_TREEWALK);
    if(rank == 0 && treewalk_redis_run_cmd(getCmd,"")<0)
     {
-        LOG(LOG_ERR,"Unable to %s",getCmd);
+        LOG(PURGER_LOG_ERR,"Unable to %s",getCmd);
         return -1;
     }
   return 0;
@@ -331,8 +333,8 @@ main (int argc, char **argv)
 
     
     /* Enable logging. */
-    TREEWALK_debug_stream = stdout;
-    TREEWALK_debug_level = TREEWALK_log_level;
+    PURGER_debug_stream = stdout;
+    PURGER_debug_level = PURGER_LOG_DBG;
     int rank = CIRCLE_init(argc, argv);
 
     opterr = 0;
@@ -342,12 +344,12 @@ main (int argc, char **argv)
         {
             case 'd':
                 TOP_DIR = realpath(optarg, NULL);
-                if(rank == 0) LOG(LOG_INFO,"Using %s as a root path.",TOP_DIR);
+                if(rank == 0) LOG(PURGER_LOG_INFO,"Using %s as a root path.",TOP_DIR);
                 dir_flag = 1;
                 break;
         
             case 'l':
-                TREEWALK_debug_level = atoi(optarg);
+                PURGER_debug_level = atoi(optarg);
                 break;
 
             case 'h':
@@ -361,19 +363,19 @@ main (int argc, char **argv)
                 break;
             
             case 'r':
-                if(rank == 0) LOG(LOG_WARN,"You have specified to use restart files.");
+                if(rank == 0) LOG(PURGER_LOG_WARN,"You have specified to use restart files.");
                 restart_flag = 1;
                 break;
 
             case 't':
                 time_flag = 1;
                 expire_threshold = (float)SECONDS_PER_DAY * atof(optarg);
-                if(rank == 0) LOG(LOG_WARN,"Changed file expiration time to %.2f days, or %.2f seconds.",expire_threshold/(60.0*60.0*24),expire_threshold);
+                if(rank == 0) LOG(PURGER_LOG_WARN,"Changed file expiration time to %.2f days, or %.2f seconds.",expire_threshold/(60.0*60.0*24),expire_threshold);
                 break;
            
             case 'f':
                 force_flag = 1;
-                if(rank == 0) LOG(LOG_WARN,"Warning: You have chosen to force treewalk.");
+                if(rank == 0) LOG(PURGER_LOG_WARN,"Warning: You have chosen to force treewalk.");
                 break;
             
             case '?':
@@ -401,41 +403,41 @@ main (int argc, char **argv)
     }
     if(restart_flag && dir_flag)
     {
-        if(rank == 0) LOG(LOG_WARN, "You have told treewalk to use both checkpoint files and a directory.  You cannot combine these options.\n"
+        if(rank == 0) LOG(PURGER_LOG_WARN, "You have told treewalk to use both checkpoint files and a directory.  You cannot combine these options.\n"
                                     "If you use a directory, treewalk will start from scratch.  If you use a checkpoint file, it will start from\n"
                                     "from the data in the checkpoint files.\n");
         exit(EXIT_FAILURE);
     }
     if(time_flag == 0)
     {
-        if(rank == 0) LOG(LOG_WARN, "A file timeout value was not specified.  Files older than %.2f seconds (%.2f days) will be expired.",expire_threshold,expire_threshold/(60.0*60.0*24.0));
+        if(rank == 0) LOG(PURGER_LOG_WARN, "A file timeout value was not specified.  Files older than %.2f seconds (%.2f days) will be expired.",expire_threshold,expire_threshold/(60.0*60.0*24.0));
     }
 
     if(dir_flag == 0 && !restart_flag)
     {
          print_usage(argv);
-         if(rank == 0) LOG(LOG_FATAL, "You must specify a starting directory");
+         if(rank == 0) LOG(PURGER_LOG_FATAL, "You must specify a starting directory");
          exit(EXIT_FAILURE);
     }
 
     if(redis_hostname_flag == 0)
     {
-        if(rank == 0) LOG(LOG_WARN, "A hostname for redis was not specified, defaulting to localhost.");
+        if(rank == 0) LOG(PURGER_LOG_WARN, "A hostname for redis was not specified, defaulting to localhost.");
         redis_hostname = "localhost";
     }
 
     if(redis_port_flag == 0)
     {
-        if(rank == 0) LOG(LOG_WARN, "A port number for redis was not specified, defaulting to 6379.");
+        if(rank == 0) LOG(PURGER_LOG_WARN, "A port number for redis was not specified, defaulting to 6379.");
         redis_port = 6379;
     }
 
     for (index = optind; index < argc; index++)
-        LOG(LOG_WARN, "Non-option argument %s", argv[index]);
+        LOG(PURGER_LOG_WARN, "Non-option argument %s", argv[index]);
     REDIS = redisConnect(redis_hostname, redis_port);
     if (REDIS->err)
     {
-        LOG(LOG_FATAL, "Redis error: %s", REDIS->errstr);
+        LOG(PURGER_LOG_FATAL, "Redis error: %s", REDIS->errstr);
         exit(EXIT_FAILURE);
     }
     
@@ -473,10 +475,10 @@ main (int argc, char **argv)
     }
     if(rank == 0)
     {
-        LOG(LOG_INFO, "treewalk run started at: %s", starttime_str);
-        LOG(LOG_INFO, "treewalk run completed at: %s", endtime_str);
-        LOG(LOG_INFO, "treewalk total time (seconds) for this run: %f",difftime(time_finished,time_started));
-        LOG(LOG_INFO, "\nTotal time in process_objects: %lf\n\
+        LOG(PURGER_LOG_INFO, "treewalk run started at: %s", starttime_str);
+        LOG(PURGER_LOG_INFO, "treewalk run completed at: %s", endtime_str);
+        LOG(PURGER_LOG_INFO, "treewalk total time (seconds) for this run: %f",difftime(time_finished,time_started));
+        LOG(PURGER_LOG_INFO, "\nTotal time in process_objects: %lf\n\
                    \tRedis commands: %lf %lf\n\
                    \tStating:  %lf %lf\n\
                    \tReaddir: %lf %lf\n\
